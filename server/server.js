16a11e389cdd4b0d98cbc4d7ae21ff0b6bdd445c
Chris Netz <cnetz89@gmail.com>
2015-02-05 12:11:29 -0700

    first node demo

---
+++ server/server.js
@@ -0,0 +1,16 @@
+var onRequest = function (req, res) {
+	res.writeHead(200, {
+		'Connection': 'close',
+		'Content-Type': 'application/json',
+		'Access-Control-Allow-Origin' : '*'
+	});
+	res.end(JSON.stringify({message: 'Hello World'}));
+}
+
+var smurfs = require('./smurfs')
+// smurfs.doSomething();
+console.log(smurfs);
+
+var http = require('http');
+http.createServer(onRequest).listen(12200);
+console.log('listening on the port 12200');
---
+++ server/smurfs.js
@@ -0,0 +1,11 @@
+// exports.IQ = 3;
+
+// exports.doSomething = function () {
+// 	console.log("i'm doing something");
+// }
+
+module.exports = {
+	doSomething: function () {
+		console.log('yes');
+	}
+}
